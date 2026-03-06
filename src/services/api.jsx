6cf21769122405972eba0a0de96644655afb62da
Junyao <junyaokh822@gmail.com>
2026-03-06 13:16:01 -0500

    update baseURL for local

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://cybermart-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
 });
 
 // Add token to requests if it exists
